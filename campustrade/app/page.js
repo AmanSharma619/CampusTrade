2d720fa0ad90cab1d0f170eac681100822067b8e
aman sharma <amansharma.as536@gmail.com>
2025-04-21 02:35:17 +0530

    responsiveness + features section final

--- campustrade/app/page.js
+++ campustrade/app/page.js
@@ -2,19 +2,18 @@
 import "./page.css"
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect } from 'react'
 import {Glowbutton,Defbutton} from '@/components/Button'
 import Card from '@/components/Card'
 import { Inter,Roboto } from 'next/font/google'
-const font_inter = Inter({
-  variable: "--inter",
-  weight: ['300','400', '700'],
-  subsets: ["latin"],
-});
+import Script from "next/script"
+
 const home = () => {
   return (
     <>
-    <div className='min-h-screen flex font-bold flex-col sticky top-0 gap-5 main'  >   
+    <Script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.7/gsap.min.js" strategy="beforeInteractive"/>
+    <Script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.7/ScrollTrigger.min.js" strategy="beforeInteractive"/>
+    <div className='min-h-screen flex font-bold flex-col sticky top-0 gap-5 main '  >   
       <div className="hero w-full text-9xl mx-auto  text-white tracking-wide text-center min-h-4/5 flex flex-col gap-6 mt-14 p-9 ">
         <h1 className='text-color1 tracking-wider'>Buy. Sell. Borrow. </h1> <h2 className='text-5xl font-normal'>From One Student to Another, A Marketplace Exclusively for Your College.</h2>
 
@@ -27,8 +26,8 @@ const home = () => {
       
 
     </div>
-    <div className={`min-h-screen bg-background sticky border-t-2 border-b-2 border-white text-white features flex flex-col justify-around p-3.5 gap-3 ${font_inter.className}`}> 
-      <h1 className='text-6xl mx-auto underline font-normal tracking-wide mt-7'>Why CampusTrade?</h1>
+    <div className={`min-h-screen bg-color2 sticky  text-white features flex flex-col justify-around p-3.5 gap-3 `}> 
+      <h1 className='text-7xl mx-auto font-bold tracking-wide mt-7'>Features</h1>
       <div className={` cards mt-6`}>
         <Card/>
       </div>
--- campustrade/components/Card.jsx
+++ campustrade/components/Card.jsx
@@ -5,29 +5,29 @@ import { Recycle,ShieldCheck,MessageSquareMore,Bell,SlidersHorizontal } from 'lu
 const Card = () => {
      const data=[
         {
-           icon: <ShieldCheck size={40}/>,
+           icon: <ShieldCheck size={70}/>,
             title: "College Exclusive" ,
             desc:" Only students with verified university codes can join, ensuring a safe and local trading community."
         },
         {
-            icon:<MessageSquareMore size={40}/>,
+            icon:<MessageSquareMore size={70}/>,
              title: "In-App Chat" ,
             desc:"Communicate with students through real time chat to discuss product details, prices, meet-up times, or negotiations"
         },
         {
-          icon:<Recycle size={40}/>,
+          icon:<Recycle size={70}/>,
              title: "Borrow Instead" ,
             desc:" Rather than buying expensive new items such as a scientific calculator, borrow one for the time being."
 
         },
         {
-          icon:<Bell size={40}/>,
+          icon:<Bell size={70}/>,
              title: "Real-Time Notifications" ,
             desc:"Get alerts for new listings, chat messages, and updates on your trades."
 
         },
         {
-          icon:<SlidersHorizontal size={40}/>,
+          icon:<SlidersHorizontal size={70}/>,
              title: "Search & Filter" ,
             desc:"Find exactly what you're looking for with advanced search and filters ."
 
@@ -38,13 +38,13 @@ const Card = () => {
     {data.map((item, index) => (
       <div
         key={index}
-        className="bg-color2 w-[25vw] h-[40vh] rounded-xl flex flex-col items-center justify-start py-8 px-5 card"
+        className="bg-transparent w-[25vw] h-[40vh] rounded-xl flex flex-col items-center justify-start py-8 px-5 card"
       >
-        <div className="flex flex-col items-center gap-6 h-full justify-start">
-          <div className="icon text-center">{item.icon}</div>
+        <div className="flex flex-col items-center gap-6 h-full justify-start text-white">
+          <div className="icon text-center card-icon">{item.icon}</div>
           <div className="text flex flex-col gap-3 text-center">
-            <h1 className="text-3xl font-bold">{item.title}</h1>
-            <h2 className="text-lg">{item.desc}</h2>
+            <h1 className="text-3xl font-normal card-title">{item.title}</h1>
+            <h2 className="text-lg font-light card-desc">{item.desc}</h2>
           </div>
         </div>
       </div>
