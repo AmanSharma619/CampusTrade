b1d88ba2695bb5bc71d35b7d2446a54a23f895c1
aman sharma <amansharma.as536@gmail.com>
2025-05-03 03:22:43 +0530

    login design changes

--- campustrade/app/login/page.js
+++ campustrade/app/login/page.js
@@ -36,17 +36,21 @@ const Login = () => {
       <div className='left-div h-full w-full flex flex-col items-center justify-center gap-4' id='left'>
         {isSignup ? (
           <>
-            <h1 className='text-6xl font-bold text-white'>Create an Account</h1>
-            <form className='w-4/5 h-2/5 flex flex-col gap-5 items-center justify-center'>
+            <h1 className='text-6xl font-bold text-white text-center'>Create an Account</h1>
+            <form className='w-4/5 min-h-3/5 flex flex-col gap-5 items-center justify-center '>
               <input type="text" className='bg-color2 w-4/5 h-10 rounded-2xl text-white p-1.5' placeholder='Name' />
-              <input type="email" className='bg-color2 w-4/5 h-10 rounded-2xl text-white p-1.5' placeholder='Email' />
+              <input type="text" className='bg-color2 w-4/5 h-10 rounded-2xl text-white p-1.5' placeholder='Section' />
+              <input type="text"  className='bg-color2 w-4/5 h-10 rounded-2xl text-white p-1.5' placeholder='Passing Year' />
+              <input type="email" className='bg-color2 w-4/5 h-10 rounded-2xl text-white p-1.5' placeholder='Student Email' />
               <input type="password" className='bg-color2 w-4/5 h-10 rounded-2xl text-white p-1.5' placeholder='Password' />
+              <span className='text-color2'>Upload College ID Card</span>
+              <input type="file" className='bg-color2 w-4/5 h-10 rounded-2xl text-white p-1.5' accept='image/*,.pdf' />
               <Glowbutton title="Sign Up" />
             </form>
           </>
         ) : (
           <>
-            <h1 className='text-6xl font-bold'>Login to Your Account</h1>
+            <h1 className='text-6xl font-bold text-center'>Login to Your Account</h1>
             <form className='w-4/5 h-2/5 flex flex-col gap-5 items-center justify-center'>
               <input type="text" className='bg-color2 w-4/5 h-10 rounded-2xl text-white p-1.5' placeholder='Email' />
               <input type="password" className='bg-color2 w-4/5 h-10 rounded-2xl text-white p-1.5' placeholder='Password' />
@@ -60,14 +64,14 @@ const Login = () => {
       <div className='right-div bg-color1 h-full w-full flex flex-col items-center justify-center gap-4' id='right'>
         {isSignup ? (
           <>
-            <h1 className='text-white text-7xl font-bold'>Welcome Back</h1>
-            <h2 className='text-2xl  font-normal'>Already have an account? Login now</h2>
+            <h1 className='text-white text-7xl font-bold text-center'>Welcome Back</h1>
+            <h2 className='text-2xl  font-normal text-center'>Already have an account? Login now</h2>
             <Defbutton title="Login" className="bg-white" onClick={toggleToLogin} />
           </>
         ) : (
           <>
-            <h1 className='text-white text-7xl font-bold'>New Here?</h1>
-            <h2 className='text-2xl text-color2 font-normal'>Sign Up and Discover The Marketplace</h2>
+            <h1 className='text-white text-7xl font-bold text-center'>New Here?</h1>
+            <h2 className='text-2xl text-color2 font-normal text-center'>Sign Up and Discover The Marketplace</h2>
             <Defbutton title="Sign Up" className="bg-white" onClick={toggleToSignup} />
           </>
         )}
